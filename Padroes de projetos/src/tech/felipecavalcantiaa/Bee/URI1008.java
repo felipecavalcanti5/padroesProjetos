5419b570c8425fcef64cc9141c6988696b38d99c
felipecavalcanti5 <2022211510016@iesp.edu.br>
2023-09-14 23:34:58 -0300

    URI 1008 added

--- Padroes de projetos/src/tech/felipecavalcantiaa/Bee/URI1008.java
+++ Padroes de projetos/src/tech/felipecavalcantiaa/Bee/URI1008.java
@@ -1,4 +1,18 @@
 package tech.felipecavalcantiaa.Bee;
+import java.util.Scanner;
 
 public class URI1008 {
+    public static void main(String[] args) {
+        int id, horasTrabalhadas;
+        double salarioHora;
+
+        Scanner scan = new Scanner(System.in);
+        id = scan.nextInt();
+        horasTrabalhadas = scan.nextInt();
+        salarioHora = scan.nextDouble();
+
+        System.out.println("NUMBER = " + id);
+        System.out.println(String.format("SALARY = U$ %.2f", horasTrabalhadas * salarioHora));
+
+    }
 }
