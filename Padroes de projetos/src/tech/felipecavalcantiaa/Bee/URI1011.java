972d3639cca2fedb09d6480678d05b7baecb01ce
felipecavalcanti5 <2022211510016@iesp.edu.br>
2023-09-16 13:34:02 -0300

    URI1011 added

---
+++ Padroes de projetos/src/tech/felipecavalcantiaa/Bee/URI1011.java
@@ -0,0 +1,16 @@
+package tech.felipecavalcantiaa.Bee;
+import java.util.Scanner;
+public class URI1011 {
+    public static void main(String[] args) {
+        double pi = 3.14159;
+        double r;
+
+        Scanner scan = new Scanner(System.in);
+        r = scan.nextDouble();
+
+        double volume = (4/3.0)* pi * Math.pow(r, 3);
+
+        System.out.println(String.format("VOLUME = %.3f", volume));
+
+    }
+}
