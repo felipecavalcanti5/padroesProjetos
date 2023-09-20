1a23f41fac489422293aeed2d9c95c32f07c42c9
Felipe Cavalcanti <112189993+felipecavalcanti5@users.noreply.github.com>
2023-09-19 21:00:19 -0300

    Update URI1070.java

--- Padroes de projetos/src/tech/felipecavalcantiaa/Bee/URI1070.java
+++ Padroes de projetos/src/tech/felipecavalcantiaa/Bee/URI1070.java
@@ -1,7 +1,18 @@
 package tech.felipecavalcantiaa.Bee;
 
+import java.util.Scanner;
+
 public class URI1070 {
     public static void main(String[] args) {
+        int x;
 
+        Scanner scan = new Scanner(System.in);
+        x = scan.nextInt();
+
+        for(int i=x; i<=x+11; i++){
+            if(i%2!=0){
+                System.out.println(i);
+            }
+        }
     }
 }
