6b5ea31eed4e2c04231f57053bb206d0286c54db
Felipe Cavalcanti <112189993+felipecavalcanti5@users.noreply.github.com>
2023-09-19 21:26:35 -0300

    Update URI1013.java

--- Padroes de projetos/src/tech/felipecavalcantiaa/Bee/URI1013.java
+++ Padroes de projetos/src/tech/felipecavalcantiaa/Bee/URI1013.java
@@ -1,16 +1,21 @@
 package tech.felipecavalcantiaa.Bee;
-
 import java.util.Scanner;
 
 public class URI1013 {
     public static void main(String[] args) {
+        
         int a, b, c;
         Scanner scan = new Scanner(System.in);
         a = scan.nextInt();
         b = scan.nextInt();
         c = scan.nextInt();
 
-        int MaiorAB = ((a+b)+(a-b))/2;
-        System.out.println(MaiorAB + " eh o maior");
+        if (a > b && a > c) {
+            System.out.println(a + " eh o maior");
+        }else if(b > a && b > c){
+            System.out.println(b + " eh o maior");
+        }else if(c > a && c > b){
+            System.out.println(c + " eh o maior");
+        }
     }
 }
