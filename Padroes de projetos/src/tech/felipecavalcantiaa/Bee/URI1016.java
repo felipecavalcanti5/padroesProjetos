30b5ab9be79a94212caaee23cc6285751d640098
felipecavalcanti5 <2022211510016@iesp.edu.br>
2023-09-21 15:11:23 -0300

    URI1016 added

---
+++ Padroes de projetos/src/tech/felipecavalcantiaa/Bee/URI1016.java
@@ -0,0 +1,13 @@
+package tech.felipecavalcantiaa.Bee;
+
+import java.util.Scanner;
+
+public class URI1016 {
+    public static void main(String[] args) {
+        int distance;
+        Scanner scan = new Scanner(System.in);
+        distance = scan.nextInt();
+
+        System.out.println(distance*2 + " minutos");
+    }
+}
